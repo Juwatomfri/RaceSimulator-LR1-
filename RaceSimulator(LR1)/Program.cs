c7d4232dc692f789add76d15d073701c5559e2bc
Juwatomfri <Anyagavriliuk@gmail.com>
2024-09-24 21:22:48 +0300

    Add project files.

---
+++ RaceSimulator(LR1)/Program.cs
@@ -0,0 +1,15 @@
+﻿using BananaJuice;
+
+namespace RaceSimulator_LR1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle flyingShip = new(speed: 5, name: "baba");
+            NewVehicle flyingShip = new(speed: 5, name: "baba");
+
+            Console.WriteLine(flyingShip);
+        }
+    }
+}
---
+++ RaceSimulator(LR1)/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceSimulator_LR1_
+{
+    public class Vehicle
+    {
+        public string Name { get; private set; }
+
+        public double Speed { get; private set; }
+
+        public Vehicle(double speed, string name)
+        {
+            Speed = speed;
+            Name = name;
+        }
+    }
+}
